9b61fc8b58018c2a995e9b50b3cc9b68ac68083c
LanluZ <LanluZ@qq.com>
2025-05-26 13:02:23 +0800

    添加 进度条显示的功能

--- select_object.py
+++ select_object.py
@@ -1,6 +1,8 @@
 import os
 import cv2
+import shutil
 import pandas as pd
+from tqdm import tqdm
 
 
 def main():
@@ -8,30 +10,43 @@ def main():
     img_path = 'temp'
     selected_path = 'selected'
 
+    # 清空文件夹
+    if os.path.exists(selected_path):
+        shutil.rmtree(selected_path)
+    os.mkdir(selected_path)
+    if os.path.exists(img_path):
+        shutil.rmtree(img_path)
+    os.mkdir(img_path)
+
     # 切割视频
-    split_video('4_new.mp4', 'temp')
+    split_video('video/24-oil.mp4', 'temp')
 
     files = os.listdir(path)
-    for file in files:
-        if file.endswith('.csv'):
-            # 创建文件夹
-            if not os.path.exists(os.path.join(selected_path, file)):
-                os.mkdir(os.path.join(selected_path, file))
+    with tqdm(
+            total=len(files),
+            desc='裁剪中',
+    ) as pbar:
+        for file in files:
+            pbar.update(1)
+            if file.endswith('.csv'):
+                # 创建文件夹
+                if not os.path.exists(os.path.join(selected_path, file)):
+                    os.mkdir(os.path.join(selected_path, file))
 
-            df = pd.read_csv(os.path.join(path, file), header=0)
+                df = pd.read_csv(os.path.join(path, file), header=0)
 
-            # 遍历时间帧
-            for i in range(df.shape[0]):
-                time = int(df.loc[i, 'time'])
-                xmin = int(df.loc[i, 'xmin'])
-                ymin = int(df.loc[i, 'ymin'])
-                xmax = int(df.loc[i, 'xmax'])
-                ymax = int(df.loc[i, 'ymax'])
+                # 遍历时间帧
+                for i in range(df.shape[0]):
+                    time = int(df.loc[i, 'time'])
+                    xmin = int(df.loc[i, 'xmin'])
+                    ymin = int(df.loc[i, 'ymin'])
+                    xmax = int(df.loc[i, 'xmax'])
+                    ymax = int(df.loc[i, 'ymax'])
 
-                # 裁剪图片
-                img = cv2.imread(os.path.join(img_path, str(time) + '.png'))
-                img = img[ymin:ymax, xmin:xmax]
-                cv2.imwrite(os.path.join(selected_path, file, str(time) + '.png'), img)
+                    # 裁剪图片
+                    img = cv2.imread(os.path.join(img_path, str(time) + '.png'))
+                    img = img[ymin:ymax, xmin:xmax]
+                    cv2.imwrite(os.path.join(selected_path, file, str(time) + '.png'), img)
 
     # 文件夹重命名
     dirs = os.listdir(selected_path)
