f12cdf47d96568a032adc422049324e7e0b5c546
LanluZ <LanluZ@qq.com>
2025-05-26 11:01:48 +0800

    1. 添加 允许接收图片文件夹的新方法

--- detect_cam.py
+++ detect_cam.py
@@ -1,25 +1,30 @@
+import os
+
 import cv2
 import shutil
 import pickle
 
 import numpy as np
+from thop.rnn_hooks import count_lstm
 
 from ultralytics import YOLO
-from boxmot import BYTETracker
+from boxmot import ByteTrack
 
 from splicing import *
 from vascular import *
 
-detect_filename = '3_new.mp4'
+detect_filename = r'D:\Users\LanluZ\Downloads\51-vapour'  # 检测视频文件或图片文件夹
+rotate_angle = cv2.ROTATE_90_COUNTERCLOCKWISE  # 视频旋转角度
+conf = 0.55  # 置信度
 
 
 def main():
     # 目标检测
     model = YOLO("best.pt")
     # 目标跟踪
-    tracker = BYTETracker()
-    # 视频对象
-    video = cv2.VideoCapture(detect_filename)
+    tracker = ByteTrack()
+    # 载入视频对象
+    video = cv2.VideoCapture(detect_filename) if not os.path.isdir(detect_filename) else DirVideo(detect_filename)
 
     # 清除文件夹
     if os.path.exists('./output'):
@@ -36,8 +41,9 @@ def main():
         ret, im = video.read()  # 帧
         if not ret:
             break
+        im = cv2.rotate(im, rotate_angle)  # 旋转
 
-        detect = model.predict(im, save=False, conf=0.6, device='cuda:0')
+        detect = model.predict(im, save=False, conf=conf, device='cuda:0')
         box_xyxy = detect[0].boxes.xyxy  # 坐标
         box_conf = detect[0].boxes.conf  # 置信度
         box_cls = detect[0].boxes.cls  # 类别
@@ -74,6 +80,28 @@ def main():
 
     # 合成视频
     splicing_video('output', 'video/' + detect_filename, 15)
+
+
+# 文件夹视频类(目的兼容cv2.VideoCapture)
+class DirVideo:
+    def __init__(self, dirpath):
+        self.dirpath = dirpath
+        self.im_filename = os.listdir(dirpath)
+        self.im_len = len(self.im_filename)
+        self.count = 0  # 伪帧数计数器
+
+    # 读取帧
+    def read(self):
+        # 检测是否无效帧
+        if self.count >= self.im_len:
+            return False, None
+        # 载入帧
+        im = cv2.imread(os.path.join(self.dirpath, self.im_filename[self.count]))
+        self.count += 1
+        return True, im
+
+    def release(self):
+        pass
 
 
 if __name__ == '__main__':
